a8b3393fd789495d3d13bf7992b5f70f6430b31c
omiug <jonathan.boivin22@gmail.com>
2015-02-03 13:56:05 +0100

    user Admin

--- src/Cine/UserBundle/Admin/UserAdmin.php
+++ src/Cine/UserBundle/Admin/UserAdmin.php
@@ -12,23 +12,72 @@ class UserAdmin extends Admin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('username', null, array('label' => 'Post Title'))
-        ;
+            ->tab("Général")
+                ->with('Identitée', array('class' => 'col-xs-6'))
+                    ->add('civilite', 'choice', array(
+                        'label' => 'Civilite',
+                        'choices' => array('Mr' => 'Mr', 'Mme' => 'Mme'),
+                        'multiple' => false,
+                        'expanded' => false
+                    ))
+                    ->add('nom', 'text', array(
+                        'label' => 'Nom'
+                    ))
+                    ->add('prenom', 'text', array(
+                        'label' => 'Prénom'
+                    ))
+                ->end()
+                ->with('Autres', array('class' => 'col-xs-6'))
+//                    ->add('familles', null, array(
+//                        'label' => "Famille d'utilisateur",
+//                        "property" => "nom",
+//                        'required' => true
+//                    ))
+                    ->add('enabled', null, array(
+                        'label' => 'Actif',
+                        'required' => false
+                    ))
+                ->end()
+            ->end()
+            ->tab("Droits")
+                ->with('Communauté des services')
+                    ->add('groups', 'sonata_type_model', array(
+                        'property' => 'name',
+                        'expanded' => true,
+                        'multiple' => true,
+                        'btn_add' => true
+                    ))
+                ->end()
+            ->end()
+            ->tab("Accès")
+                ->with('Accès')
+                    ->add('username', 'text', array(
+                        'label' => 'Pseudo'
+                    ))
+                    ->add('email', 'text', array(
+                        'label' => 'E-mail'
+                    ))
+                    ->add('plainPassword', 'text', array(
+                        'label' => 'Mot de passe'
+                    ))
+                ->end()
+            ->end();
     }
 
-    // Fields to be shown on filter forms
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
         $datagridMapper
-            ->add('username')
+                ->add('username')
+                ->add('nom')
+//                ->add('familles')
+                ->add('enabled', null, array('label' => 'Actif'))
         ;
     }
 
-    // Fields to be shown on lists
-    protected function configureListFields(ListMapper $listMapper)
-    {
+    protected function configureListFields(ListMapper $listMapper) {
         $listMapper
-            ->addIdentifier('username')
+                ->addIdentifier('username')
+                ->add('nom')
+                ->add('enabled', null, array('label' => 'Actif'))
         ;
     }
 }
